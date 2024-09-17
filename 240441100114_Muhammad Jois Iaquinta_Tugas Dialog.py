28f5760e587f83dfed40d83dedf98840bcb5595c
IM JOIS <71706340+o320@users.noreply.github.com>
2024-09-17 22:00:01 +0700

    Add files via upload

---
+++ 240441100114_Muhammad Jois Iaquinta_Tugas Dialog.py
@@ -0,0 +1,51 @@
+namaku = "jois"
+namamu = "andareas"
+prodiku = "sistem informasi"
+prodimu = "teknik mesin"
+matkulku = "alpro"
+matkulmu = "pengantar teknik mesin"
+
+print("suatu hari saat bonding ospek pedona ada 2 mahasiswa yang satu klompok dan merekapun berkenalan satu sama lain")
+input("tekan next untuk dialog selanjutnya")
+
+print("jois : halo apakah kita boleh kenalan?")
+print("andreas : oh jelas tentu saja boleh")
+
+print("jois : salam kenal ya namaku", namaku ,"dan kamu?")
+print("andreas : salam kenal juga aku", namamu)
+
+print("jois : kamu dari prodi apa?")
+print("andreas :aku dari prodi", prodimu ,"kalau kamu")
+print("jois : kalo aku dari prodi" ,prodimu)
+
+print("jois : btw matkul apa yang kamu sukai?")
+print("andreas : matkul yang saya sukai yaitu matkul", matkulmu ,"dan kamu")
+print("jois : oalaa,kalo matkul yang aku sukai yaitu matkul" ,matkulku)
+
+print("jois : eh kapan-kapan ngopi yuk")
+jawab = input("masukkan jawaban")
+
+if jawab == "boleh" : 
+    print("andreas : ngopi dimana")
+else : 
+    print("andreas : iya kalau ga sibuk")
+
+print("setelah beberapa hari mereka bertemu dan menentukan tempat")
+print("jois : eh ini enaknya ngopi dimana")
+print("andreas : gimana kalau di swarna?")
+print("jois : gass")
+
+print("setelahnya mereka sampai di swarna dan merekapun memesan")
+
+barang1 = input("jois : eh kamu mau pesan apa?")
+print("andreas : aku mau" , barang1)
+barang2 = input("andreas : kamu mau apa")
+print("jois : aku mau" , barang2)
+ 
+barang1 = 8000
+barang2 = 10000
+print("kasir : ini totalnya" , barang1 + barang2)
+print("mereka langsung membayarnya dan ngopi bersama")
+print("TAMATTTTT")
+
+
